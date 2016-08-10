acd892e5a33e393a934d00839e4b776712166a42
Hugo Hoo <hughoo@outlook.com>
2016-08-10 18:51:23 +0800

    index.html DONE.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.static("public"));
 app.set("views", __dirname + "/views");
 
 app.get("/", function(req, res){
-    res.send("<h2>Socket.io Chat</h2>");
+    res.sendFile("index.html", { root : "./views" });
 });
 
 app.listen(3000);
