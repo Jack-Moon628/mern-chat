8fafec707b7bc4d69f27f0dbaf122569a986313b
Hugo Hoo <hughoo@outlook.com>
2016-08-11 09:09:09 +0800

    socket.io connected(just connected)

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let express = require("express");
-
 let app = express();
+let server = require("http").createServer(app);
+let io = require("socket.io")(server);
+
 
 app.use(express.static("public"));
 app.set("views", __dirname + "/views");
@@ -9,5 +11,9 @@ app.get("/", function(req, res){
     res.sendFile("index.html", { root : "./views" });
 });
 
-app.listen(3000);
+io.on("connection", function(socket){
+    console.log("user connected.");
+});
+
+server.listen(3000);
 console.log("Listening on http://*:3000");
