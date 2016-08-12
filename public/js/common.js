9984408308980c81c6ce4b2ee6adbe596dad8bea
Hugo Hoo <hughoo@outlook.com>
2016-08-12 11:25:49 +0800

    add : send button

--- public/js/common.js
+++ public/js/common.js
@@ -2,45 +2,52 @@
 let app = angular.module("app", []);
 app.controller("msgListCtrl", function($scope){
     $scope.users = [];
-
-    //socket.io
-    let socket = io();
-
-    $("#msg-input").keydown(function(event){
-        if(event.keyCode == 13){
-            let msg = $(this).val();
-            if(msg == "") return;
-            socket.send(msg);
-            $(this).val("");
-        }
-    })
-
-    socket.on("message", function(msg){
-        let $elem = angular.element('[ng-controller=msgListCtrl]');
-        // alert($elem.attr("id"));
-        let $scope = $elem.scope();
-        $scope.users.push({
-            name : msg.name,
-            time : msg.time,
-            msg : msg.msg
-        });
-        $scope.$apply();
-
-        // scroll to the end of message list
-        let liElem = $("#msg-list").children().last().get(0);
-        liElem.scrollIntoView({block: "end", behavior: "smooth"});
-    });
-
-    socket.on("system", function(msg){
-        let $elem = angular.element('[ng-controller=msgListCtrl]');
-        // alert($elem.attr("id"));
-        let $scope = $elem.scope();
-        $scope.users.push({
-            name : msg.name,
-            time : msg.time,
-            msg : msg.msg
-        });
-        $scope.$apply();
-    })
-
 });
+
+//socket.io
+let socket = io();
+
+$("#msg-input").keydown(function(event){
+    if(event.keyCode == 13){
+        sendMsg($(this));
+    }
+});
+
+$("#msg-sendbtn").click(function(event){
+    sendMsg($("#msg-input"));
+});
+
+function sendMsg($inputElem){
+    let msg = $inputElem.val();
+    if(msg == "") return;
+    socket.send(msg);
+    $inputElem.val("");
+}
+
+socket.on("message", function(msg){
+    let $elem = angular.element('[ng-controller=msgListCtrl]');
+    // alert($elem.attr("id"));
+    let $scope = $elem.scope();
+    $scope.users.push({
+        name : msg.name,
+        time : msg.time,
+        msg : msg.msg
+    });
+    $scope.$apply();
+
+    // scroll to the end of message list
+    let liElem = $("#msg-list").children().last().get(0);
+    liElem.scrollIntoView({block: "end", behavior: "smooth"});
+});
+
+socket.on("system", function(msg){
+    let $elem = angular.element('[ng-controller=msgListCtrl]');
+    // alert($elem.attr("id"));
+    let $scope = $elem.scope();
+    $scope.users.push({
+        name : msg.name,
+        time : msg.time,
+        msg : msg.msg
+    });
+    $scope.$apply();
+})
