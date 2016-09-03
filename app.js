f54dfee8394f38581dbf7e1ea1df1ee93b515755
hugohoo <hughoo@outlook.com>
2016-09-03 21:13:35 +0800

    improving private msg

--- app.js
+++ app.js
@@ -95,8 +95,8 @@ io.on("connection", function(socket){
             }
 
             if(obj.private == 1){
-                for (var i = 0; i < clientList.length; i++) {
-                    if(obj.target[i].hasOwnProperty("isPrivateTgt") == true && obj.target[i].isPrivateTgt == true){
+                for (var i = 0; i < obj.target.length; i++) {
+                    if(obj.target[i] && obj.target[i].hasOwnProperty("isPrivateTgt") == true && obj.target[i].isPrivateTgt == true){
                         let name = obj.target[i].name;
                         let index = getIndex(clientList, "name", name);
                         console.log(util.inspect(clientList[index], false, 1, true));
